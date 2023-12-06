ef2ea447db09fd173164d2eeb87746f8291831e4
nlunce <naterrl15@byui.edu>
2023-12-05 21:27:30 -0700

    2

--- src/Pages/Quiz.jsx
+++ src/Pages/Quiz.jsx
@@ -1,7 +1,11 @@
 import { Quiz } from "../ui-components";
+import { useState } from "react";
 
 const QuizPage = () => {
-  const overrides = {};
+  const [question, setQuestion] = useState();
+  // const [question, setQuestion] = useState();
+
+  const overrides = { Question: { children: question } };
 
   return <Quiz overrides={overrides} />;
 };
